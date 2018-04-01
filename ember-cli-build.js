3191a112be91efd8e17e652470d5430ce124e796
Chris Manson <chris@stonecircle.io>
2018-04-01 17:45:47 +0100

    updating ember-casper-template to allow it to take over prember

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,18 +2,11 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-const urls = [
-  '/'
-];
-
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     fingerprint: {
       prepend: `http://www.leedsjs.com.s3-website-eu-west-1.amazonaws.com/`,
       extensions: ['js', 'css', 'map']
-    },
-    prember: {
-      urls
     },
   });
 
