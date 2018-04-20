4168cd80c8490761431cc95d15147d5ef8aa4658
Chris Manson <chris@stonecircle.io>
2018-04-20 12:04:22 +0100

    fixing deployment

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,6 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     fingerprint: {
-      prepend: `http://www.leedsjs.com.s3-website-eu-west-1.amazonaws.com/`,
       extensions: ['js', 'css', 'map']
     },
   });
